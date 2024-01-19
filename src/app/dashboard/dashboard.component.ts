388cd7c26bafd28b3513fab6973be937a3bd1f91
Pereira De Paiva <fernando.pereira@transfesa.com>
2024-01-19 12:27:08 +0100

    Configuración del dashboard - Tailwind

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../shared/side-menu/side-menu.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SideMenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   standalone: true,
+  selector: 'app-side-menu',
   imports: [],
   templateUrl: './side-menu.component.html',
   styles: ``
 })
 export class SideMenuComponent {
+
+  constructor() {
+
+    public menuItems = routes
+      .map( route => route.children ?? [] )
+      .flat()
+      .filter( route => route && route.path )
+      .filter( route => !route.path?.includes(':') );
+
+    // const dashboardRoutes = routes
+    //   .map( route => route.children ?? [] )
+    //   .flat()
+    //   .filter( route => route && route.path )
+    //   .filter( route => !route.path?.includes(':') )
+
+    // console.log( dashboardRoutes );
+  }
 
 }
