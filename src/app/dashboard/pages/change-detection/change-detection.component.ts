a5f9e5fa3264133d1e1712e5ed850bcb2c3e403f
Pereira De Paiva <fernando.pereira@transfesa.com>
2024-01-22 12:07:17 +0100

    Seguimiento

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template: `
+
+    <app-title title="Change Detection" />
+
+    <pre>{{ frameworkAsSignal() | json }}</pre>
+    <pre>{{ frameworkAsProperty | json }}</pre>
+
+
+
+  `
 })
 export default class ChangeDetectionComponent {
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  })
+
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+//   constructor() {
+//     setTimeOut(() => {
+
+//   }, 3000);
+// }
 
 }
