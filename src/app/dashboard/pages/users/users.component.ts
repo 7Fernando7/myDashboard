9a931281ea77b1ec581fa629de245971763838b3
Fernando <info_fotografia@icloud.com>
2024-02-24 18:25:05 +0100

    Servicios con señales

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
   imports: [],
   templateUrl: './users.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class UsersComponent {
-
+  public usersService = inject(UsersService);
 }
